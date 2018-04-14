f9d7b6970f554eedf67a2486ca8a41d94ec657be
dm13y <5132095@tut.by>
2018-04-14 22:32:10 +0300

    spring security core

---
+++ src/main/java/by/dm13y/study/springsecurity/core/AuthenticationExample.java
@@ -0,0 +1,41 @@
+package by.dm13y.study.springsecurity.core;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * https://ru.wikibooks.org/wiki/Spring_Security/%D0%A2%D0%B5%D1%85%D0%BD%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B9_%D0%BE%D0%B1%D0%B7%D0%BE%D1%80_Spring_Security#%D0%9F%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B8%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%86%D0%B8%D0%B8_%D0%BE_%D1%82%D0%B5%D0%BA%D1%83%D1%89%D0%B5%D0%BC_%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D0%B5
+ */
+public class AuthenticationExample {
+    private static AuthenticationManager am = new SampleAuthenticationManager();
+
+    public static void main(String[] strings) throws Exception{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true) {
+            System.out.println("Please enter your username:");
+            String name = in.readLine();
+            System.out.println("Please enter your password:");
+            String password = in.readLine();
+
+            try {
+                Authentication request = new UsernamePasswordAuthenticationToken(name, password);
+                Authentication result = am.authenticate(request);
+                SecurityContextHolder.getContext().setAuthentication(result);
+                break;
+            } catch (AuthenticationException e) {
+                System.out.println("Authentication failed: " + e.getMessage());
+            }
+        }
+        System.out.println("Successfully authenticated. Security context contains: " +
+                SecurityContextHolder.getContext().getAuthentication());
+
+    }
+}
---
+++ src/main/java/by/dm13y/study/springsecurity/core/SampleAuthenticationManager.java
@@ -0,0 +1,26 @@
+package by.dm13y.study.springsecurity.core;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SampleAuthenticationManager implements AuthenticationManager {
+    static final List<GrantedAuthority> AUTHORITIES = new ArrayList<GrantedAuthority>();
+    static {
+        AUTHORITIES.add(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+    @Override
+    public Authentication authenticate(Authentication auth) throws AuthenticationException {
+        if(auth.getName().equals(auth.getCredentials())){
+            return new UsernamePasswordAuthenticationToken(auth.getName(), auth.getCredentials(), AUTHORITIES);
+        }
+        throw new BadCredentialsException("Bad Credential");
+    }
+}
